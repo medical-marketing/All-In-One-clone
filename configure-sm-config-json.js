2ec594326279aae97fc376ab7dc71b9fcc5e0640
templar-ajay <templar.ajay@gmail.com>
2024-03-04 22:02:22 +0530

    change: config file

--- configure-sm-config-json.js
+++ configure-sm-config-json.js
@@ -1,23 +1,40 @@
-// update-config.js
 require("dotenv").config();
 const fs = require("fs");
 
 const configFile = "slicemachine.config.json";
 
+// Get the repository name from the environment variable
+const prismicRepoName = process.env.PRISMIC_REPO_NAME;
+
+if (!prismicRepoName) {
+  console.error(
+    "Error: PRISMIC_REPO_NAME environment variable is not provided. Stopping the build process."
+  );
+  process.exit(1);
+}
+
 try {
+  // Check if the file already exists
+  if (!fs.existsSync(configFile)) {
+    const content = `{
+    "repositoryName": "${prismicRepoName}",
+    "adapter": "@slicemachine/adapter-next",
+    "libraries": [
+      "./src/slices"
+    ],
+    "localSliceSimulatorURL": "http://localhost:3000/slice-simulator"
+  }`;
+
+    // Create the file
+    fs.writeFileSync(configFile, content);
+    console.log("File created successfully.");
+  } else {
+    console.log("File already exists. No action needed.");
+  }
+
   // Read the existing configuration file
   const configData = fs.readFileSync(configFile, "utf8");
   const config = JSON.parse(configData);
-
-  // Get the repository name from the environment variable
-  const prismicRepoName = process.env.PRISMIC_REPO_NAME;
-
-  if (!prismicRepoName) {
-    console.error(
-      "Error: PRISMIC_REPO_NAME environment variable is not provided. Stopping the build process."
-    );
-    process.exit(1);
-  }
 
   // Update the repositoryName in the config
   config.repositoryName = prismicRepoName;
